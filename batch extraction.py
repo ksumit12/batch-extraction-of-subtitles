d5ba7fae6f9ab1fa81f4a9c821480206575f1ef4
ksumit12 <70723407+ksumit12@users.noreply.github.com>
2021-09-10 15:30:33 +0530

    Update batch extraction.py

--- batch extraction.py
+++ batch extraction.py
@@ -8,82 +8,103 @@ Copyright (c) 2021 DREAM innovators
 '''
 
 
-
-#!/usr/bin/python
+# !/usr/bin/python
 import subprocess
 import argparse
 import os
 import time
+import sys
 
 start_time = time.time()
+jst=[]
 
-def get_subtitle(input_path,output_path):
+
+
+
+def get_subtitle(input_path ,output_path):
     name_list = []
     srt_name = []
     path = os.listdir(input_path)
-    for file in path[1:]:
-
-        name = file[:file.rfind(".")]
-        name_list.append(name)
-        final = name + '[cleaned]' + '.srt'
-        srt_name.append(final)
-        # print(final)
-        cm = f'ffmpeg -i ' + '"' + input_path + "\\" + file + '"' + " " + '"' + output_path + "\\" + final + '"'
-        # print(cm)
-        pt = subprocess.run(cm, shell=True)
+    for file in path:
+        if file.endswith(('.mp4' ,'.MP4' ,'mkv')):
+            name = file[:file.rfind(".")]
+            name_list.append(name)
+            final = name + '[Unprocessed]' + '.srt'
+            srt_name.append(final)
+            # print(final)
+            cm = f'ffmpeg -i ' + '"' + input_path + "\\" + file + '"' + " " + '"' + output_path + "\\" + final + '"'
+            # print(cm)
+            pt = subprocess.run(cm, shell=True)
+        else:
+            print("***************it's not a video so can't be converted********" ,file)
 
     if pt.returncode == 0:
         print('command successfully executed!')
     else:
         print(' command failed')
 
-    return name_list,srt_name
+    return name_list ,srt_name
 
 
-def srt(file_name, srt_name):
+def srt(path):
+    lst=[]
+    directory = "Processed-SRT"
+    final_dir = os.path.join(out_path, directory)
+    # print("path", path)
+    try:
+        os.mkdir(final_dir)
+    except:
+        pass
 
-    for x in srt_name:
+    for p in path:
+        if p.endswith(('.srt','.SRT')):
+            input = f"{out_path}\{p}"
+            # print("input", input)
+            output = f"{final_dir}\{p[:len(p) - 4]}-PROCESSED.SRT"
 
-      cleaned_srt = out_path + "\\" + x
-      print('cleaned srt',cleaned_srt)
-      a = open(cleaned_srt, "r")
-      k = a.readlines()
-    for z in file_name:
-       srt_name = out_path+"\\"+"final"+"\\"+z+"[final]"+".srt"
-       print('srt name',srt_name)
-       b = open(srt_name, "a")
-       print('a',a)
-       print('b',b)
+            print("output", output)
+
+            a = open(input, "r")
+            b = open(output, "w")
+            lst.append(output)
+
+            k = a.readlines()
+            global jst
+            jst=lst
 
 
-    for line in k:
-        if line[0] == "F":
-            d = line.find("GPS")
-            b.write("LAT " + line[d + 5:d + 12] + "\n")
-            # print("LAT " + line[d + 5:d + 12] + "\n")
-            b.write("LON " + line[d + 14:d + 21] + "\n")
-            # print("LON " + line[d + 14:d + 21] + "\n")
+            for line in k:
+                if line[0] == "F":
+                    d = line.find("GPS")
+                    b.write("LAT " + line[d + 5:d + 12] + "\n")
+                    b.write("LON " + line[d + 14:d + 21] + "\n")
+                else:
+                    b.write(line)
+
+            a.close()
+            b.close()
+            print(f"{p} Successfully done!")
         else:
-            b.write(line)
-            # print(line)
+            print("it's not a srt file")
 
-    a.close()
-    b.close()
+
 
 
 if __name__ == "__main__":
-   parser = argparse.ArgumentParser()
-   parser.add_argument("--input_path", help="full path of input file")
-   parser.add_argument("--output_path", help="full path of desired output folder")
-   args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_path", help="full path of input file")
+    parser.add_argument("--output_path", help="full path of desired output folder")
+    args = parser.parse_args()
 
-   in_path = args.input_path
-   out_path =args.output_path
+    in_path = args.input_path
+    out_path =args.output_path
 
-   name,SRT_name = get_subtitle(in_path,out_path)
-   time.sleep(20)
-   srt(name,SRT_name)
+    name, SRT_name = get_subtitle(in_path, out_path)
+    time.sleep(5)
+
+    # for s in range(1,length): print("no of iter",s)
+    final = srt( SRT_name)
 
 
-
-   print("--- %s seconds ---" % (time.time() - start_time))
+   
+    print("--- %s seconds ---" % (time.time() - start_time))
